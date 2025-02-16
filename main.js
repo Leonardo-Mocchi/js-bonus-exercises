002f430b0325a5684db1468c9afa937bac48c2bf
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-16 15:19:19 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("ESERCIZIO 1");
+
+console.log();
+console.log("ESERCIZIO 2");
+
+console.log();
+console.log("ESERCIZIO 3");
+
+console.log();
+console.log("ESERCIZIO 4");
+
+console.log();
+console.log("ESERCIZIO 5");
+
+console.log();
+console.log("ESERCIZIO 6");
+
+console.log();
+console.log("ESERCIZIO 7");
+
+console.log();
+console.log("ESERCIZIO 8");
+
+console.log();
+console.log("ESERCIZIO 9");

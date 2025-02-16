2317a0b3bed0e6b5eb6022f80a6a6063cec5408b
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-16 16:35:37 +0100

    add exercise 3 done

--- main.js
+++ main.js
@@ -20,30 +20,43 @@ console.log("The maximum number of the array is: " + numArrMax);
 console.log();
 console.log("ESERCIZIO 2");
 
-/* let userInput = prompt("Type anything and I will count the vowels inside") */
-let userInput = "banane e arzigugoli a forma di banane"
+/* const userInput = prompt("Type anything and I will count the vowels inside") */
+const userInput = "banane e arzigugoli a forma di banane"
 console.log("You wrote: " + userInput);
 
 function countVowels(string) {
-    let As = string.match(/a/gi);
-    let Es = string.match(/e/gi);
-    let Is = string.match(/i/gi);
-    let Os = string.match(/o/gi);
-    let Us = string.match(/u/gi);
+    const As = string.match(/a/gi);
+    const Es = string.match(/e/gi);
+    const Is = string.match(/i/gi);
+    const Os = string.match(/o/gi);
+    const Us = string.match(/u/gi);
 
     console.log(As, Es, Is, Os, Us);
 
     return As.length + Es.length + Is.length + Os.length + Us.length
 }
 
-let numVowels = countVowels(userInput)
+const numVowels = countVowels(userInput)
 console.log("The number of vowels of what you wrote is: " + numVowels);
 
 /* ################################################### */
 console.log();
 console.log("ESERCIZIO 3");
 
+const userArr = ["Ajo", "Eja", "Mischino", "Inugru", "Tamatta", "Marragocco", "Eliga"]
+console.log("Your array is: ", userArr);
 
+function invertArray(arr) {
+    const invertedArr = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const revertWords = arr[i];
+        invertedArr.push(revertWords)
+    }
+    return invertedArr
+}
+
+const revertUserArr = invertArray(userArr)
+console.log("The reverted array is: ", revertUserArr);
 
 /* ################################################### */
 console.log();
